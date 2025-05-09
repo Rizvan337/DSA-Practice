6563476dd370b39351710b0f8afe4c5a1939c38a
Rizvan337 <rizvanichu337@gmail.com>
2025-05-09 15:34:13 +0530

    small change

---
+++ JavaScript/Arrays/TwoSum.js
@@ -0,0 +1,26 @@
+// 1. Two Sum - Brute Force
+var twoSum = function(nums, target) {
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]===target){
+                return [i,j]
+            }
+        }
+    }
+}
+
+
+
+
+// 1. Two Sum - Hash Map
+var twoSum = function(nums, target) {
+    let map = new Map()
+    for(let i=0;i<nums.length;i++){
+        let num2 = target - nums[i]
+        if(map.has(num2)){
+            return [map.get(num2),i]
+        }
+        map.set(nums[i],i)
+    }
+    return []
+};
